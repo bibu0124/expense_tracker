03a9a1fc734f5f2768b792c0e36fe7b2bdc5bd2a
Nguyen Trong Duy <duynt0124@>
2023-09-13 10:09:46 +0700

    remove pod file lock and add example video

--- trackExpense/Pods/Target Support Files/NextLevelSessionExporter/NextLevelSessionExporter-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NextLevelSessionExporter : NSObject
-@end
-@implementation PodsDummy_NextLevelSessionExporter
-@end
--- trackExpense/Pods/Target Support Files/Pods-TrackerApp/Pods-TrackerApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_TrackerApp : NSObject
-@end
-@implementation PodsDummy_Pods_TrackerApp
-@end
